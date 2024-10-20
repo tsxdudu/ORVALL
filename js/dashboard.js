1fc4487652490a1f1a752a474d8142f76521c7a1
Matheus-Pedro <matheus.caprioli.pedro@gmail.com>
2024-10-19 21:41:20 -0300

    FIX Design Dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -33,10 +33,30 @@ fetch(url)
     console.log(error);
   });
 
-var map = L.map('map').setView([-25.32677857762768, -49.218576575247575], 15);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+var map = L.map('map', {
+  center: [-25.32677857762768, -49.218576575247575], // São Paulo
+  zoom: 10,
+  zoomControl: true, // Desativa o controle de zoom
+  dragging: false, // Desativa arrastar o mapa
+  scrollWheelZoom: false, // Desativa o zoom com a roda do mouse
+  doubleClickZoom: false, // Desativa o zoom ao dar duplo clique
+  boxZoom: false, // Desativa o zoom por seleção de caixa
+  keyboard: false // Desativa interações pelo teclado
+});
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+  maxZoom: 19
+  // ,attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">CartoCDN</a>'
 }).addTo(map);
 
-var marker = L.marker([-25.32677857762768, -49.218576575247575]).addTo(map);
+var bootstrapIcon = L.divIcon({
+  html: `<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" class="shadow" fill="currentColor" style="color: #f8ad09;" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+  <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
+</svg>`,  // ícone Bootstrap (geo-fill)
+  className: '', // Remove as classes padrão para personalizar totalmente
+  iconSize: [35, 35], // Tamanho do ícone
+  iconAnchor: [16, 32], // Âncora do ícone (ponto inferior)
+  popupAnchor: [0, -32]  // Âncora do popup
+});
+
+var marker = L.marker([-25.32677857762768, -49.218576575247575], { icon: bootstrapIcon }).addTo(map);
+
